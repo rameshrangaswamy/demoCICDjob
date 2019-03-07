fa5b919ce9b0b035249f76761301c5f029fe79d1
rameshrangaswamy <rameshrangaswamy1@gmail.com>
2019-03-07 13:04:04 +0000

    test

---
+++ pipeline/Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+node('node1') {
+  stage('checkout') {
+            steps {
+                // clone project and install dependencies
+                git url: 'https://github.com/rameshrangaswamy/demoCICDjob.git', branch: 'master'
+            }
+    stash 'name-of-the-stash'
+  }
+}
+node('node2') {
+  stage('build') {
+            steps {
+                // coverage tests initialization script
+                sh '''mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent \
+                  install -Dmaven.test.failure.ignore=true || true'''
+            }
+    unstash 'name-of-the-stash'
+    sh 'make'
+  }
+}
