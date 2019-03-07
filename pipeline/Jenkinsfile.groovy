040db0a28f52e148d41baf40945aabd2441f1384
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-07 18:39:27 +0530

    Update Jenkinsfile.groovy

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -2,20 +2,20 @@ def steps
 
 node('node1') {
   stage('checkout') {
-            steps {
+            //steps {
                 // clone project and install dependencies
                 git url: 'https://github.com/rameshrangaswamy/demoCICDjob.git', branch: 'master'
-            }
+            //}
     stash 'name-of-the-stash'
   }
 }
 node('node2') {
   stage('build') {
-            steps {
+            //steps {
                 // coverage tests initialization script
                 sh '''mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent \
                   install -Dmaven.test.failure.ignore=true || true'''
-            }
+            //}
     unstash 'name-of-the-stash'
     sh 'make'
   }
