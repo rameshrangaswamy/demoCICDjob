bc0b31db0152b15212be012c667ce1b788d4f020
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-07 20:48:19 +0530

    Update Jenkinsfile.groovy

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -10,17 +10,13 @@ node('node1')
   }
 
 //node('node1') {
-def mavenHome = tool(name: 'maven-3.6.0', type: 'maven');
-withEnv([
-            'MAVEN_HOME=' + mavenHome,
-            "PATH=${mavenHome}/bin:${env.PATH}"
-    ]) 
   stage('build') 
   {
             //steps {
                  //coverage tests initialization script
                   sh '''mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent \
                   install -Dmaven.test.failure.ignore=true || true'''
+                  git url: 'https://github.com/jenkinsci/throttle-concurrent-builds-plugin.git', branch: 'master'
             //}
     unstash 'name-of-the-stash'
     sh 'echo "test"'
