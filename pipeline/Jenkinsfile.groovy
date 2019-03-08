cbd9633ed15a5aa149833b6cf0ef2d6fc9d73a1d
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-08 16:28:43 +0530

    Update Jenkinsfile.groovy

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -2,10 +2,9 @@ stage('checkout')
 {
     throttle(['test_1']) {
     node('node1') {
-    {
+        
        git url: 'https://github.com/JeffLi1993/springboot-learning-example.git', branch: 'master'
      
-    }
         }
     }
 }
