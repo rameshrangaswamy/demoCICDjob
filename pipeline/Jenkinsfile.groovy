a0cef99270a035b51edabbf0a7b23b9076b92ad0
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-08 16:45:27 +0530

    Update Jenkinsfile.groovy

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -1,14 +1,12 @@
 def mvnHome
 mvnHome = tool 'maven'
-stage('checkout') 
+stage('checkout & build') 
 {
     throttle(['test_1']) {
     node('node1') {
                  git url: 'https://github.com/spring-projects/spring-boot.git', branch: 'master'
-stage('build')
-{
-    sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+                 sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+       } 
+    }
 }
-}
-}
-}
+
