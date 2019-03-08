273d3316fffa17b2348b5cb8b44610fdd28f430b
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-08 16:40:38 +0530

    sd

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -4,13 +4,10 @@ stage('checkout')
 {
     throttle(['test_1']) {
     node('node1') {
-
-       git url: 'https://github.com/spring-projects/spring-boot.git', branch: 'master'
-     
-        }
-    }
-}
+                 git url: 'https://github.com/spring-projects/spring-boot.git', branch: 'master'
 stage('build')
 {
     sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+}
+}
 }
