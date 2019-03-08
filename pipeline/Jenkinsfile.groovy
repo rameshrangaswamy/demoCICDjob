da5e41907f0d7949846d3a460e7bdc7a61c39689
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-08 16:34:49 +0530

    test

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -1,10 +1,16 @@
+def mvnHome
+mvnHome = tool 'maven'
 stage('checkout') 
 {
     throttle(['test_1']) {
     node('node1') {
-        
-       git url: 'https://github.com/JeffLi1993/springboot-learning-example.git', branch: 'master'
+
+       git url: 'https://github.com/spring-projects/spring-boot.git', branch: 'master'
      
         }
     }
+}
+stage('build')
+{
+    sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
 }
