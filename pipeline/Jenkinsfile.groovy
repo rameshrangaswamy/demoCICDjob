83f429157b322de27de0dd0285930ac8d7563ce2
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-13 19:22:50 +0530

    Update Jenkinsfile.groovy

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -1,10 +1,6 @@
 node ('testnode') 
 {
- /*           def mavenHome = tool(name: 'maven-3.6.0', type: 'maven')
-            withEnv([
-            'M2_HOME=' + mavenHome,
-            "PATH=${mavenHome}/bin:${env.PATH}"
-            ])*/
+
 stage('checkout')
 {
     throttle(['test_1'])
@@ -15,16 +11,21 @@ stage('checkout')
         } 
     }
 }
-
-stage('build')
-{
-    throttle(['test_1'])
-    {
-        node ('testnode') 
-        {   
-            sh '''mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true || true'''
-        
-        }
-    }         
-}
+def mavenHome = tool(name: 'maven-3.6.0', type: 'maven')
+withEnv([
+            'M2_HOME=' + mavenHome,
+            "PATH=${mavenHome}/bin:${env.PATH}"
+        ]) {
+                stage('build')
+                {
+                    throttle(['test_1'])
+                    {
+                        node ('testnode') 
+                        {   
+                            sh '''mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true || true'''
+                        
+                        }
+                    }         
+                }
+            }
 }
