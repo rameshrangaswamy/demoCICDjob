1253c962b4ac3450c68fddbf374f98912b5d549a
Ramesh Rangaswamy -X (rrangas2 - WIPRO LIMITED at Cisco) <rrangas2@partnet.cisco.com>
2019-03-13 22:47:16 +0530

    sf

---
+++ pipeline/Jenkinsfile.groovy
@@ -0,0 +1,32 @@
+node ('testnode') 
+{
+
+stage('checkout')
+{
+    throttle(['test_1'])
+    {
+        node ('testnode') 
+        {
+            checkout scm
+        } 
+    }
+}
+def mavenHome = tool(name: 'maven', type: 'maven')
+withEnv([
+            'MAVEN_HOME=' + mavenHome,
+            "PATH=${mavenHome}/bin:${env.PATH}"
+        ]) {
+                stage('build')
+                {
+                    throttle(['test_1'])
+                    {
+                        node ('testnode') 
+                        {   
+                            sh "echo $mavenHome"
+                            sh "${mavenHome}/bin/mvn clean install"
+                        
+                        }
+                    }         
+                }
+            }
+}
