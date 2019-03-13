1204ad66ba96ff3e9551b72ddf35fa956285dd38
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-13 18:54:24 +0530

    trt

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -1,10 +1,10 @@
-node ('testnode') 
+node ('node1') 
 {
 stage('checkout')
 {
     throttle(['test_1'])
     {
-        node ('testnode') 
+        node ('node1') 
         {
             checkout scm
         } 
@@ -13,7 +13,7 @@ stage('checkout')
 
 stage('build')
 {
-    def mavenHome = tool(name: 'maven-3.6.0', type: 'maven');
+    def mavenHome = tool(name: 'maven-3.6.0', type: 'maven')
     withEnv([
             'M2_HOME=' + mavenHome,
             "PATH=${mavenHome}/bin:${env.PATH}"
@@ -21,7 +21,7 @@ stage('build')
 
     throttle(['test_1'])
     {
-        node ('testnode') 
+        node ('node1') 
         {   
             sh '''mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true || true'''
         
