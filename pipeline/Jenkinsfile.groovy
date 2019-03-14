ac5b3a956fb5a73fae7c4096f829b7a5e6eabdb9
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-14 12:47:40 +0530

    qAD

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -1,22 +1,33 @@
 node ('testnode')
 {
-def mavenHome = tool(name: 'maven', type: 'maven')
-withEnv([
-            'MAVEN_HOME=' + mavenHome,
-            "PATH=${mavenHome}/bin:${env.PATH}"
-        ]){
-                stage('checkout & build')
-                {
-                    throttle(['test_1'])
-                    {
-                        node ('testnode') 
-                        {   
-                            //sh "echo $mavenHome"
-                            checkout scm
-                             sh "${mavenHome}/bin/mvn clean package"
-                        
-                        }
-                    }         
-                }
+    stage('stage1')
+    {
+            checkout scm     
+    }
+    stage('stage2')
+    {
+        throttle(['test_1'])
+        {
+            node ('testnode') 
+            {   
+                checkout scm
+            
             }
+        }         
+    }
+    stage('stage3')
+    {
+        throttle(['test_1'])
+        {
+            node ('testnode') 
+            {   
+                checkout scm
+            
+            }
+        }         
+    }
+        stage('stage4')
+    {
+         sh "echo $M2_HOME"         
+    }
 }
