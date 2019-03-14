4b09ae8bfad0dfd78ea45de10af2db1df76a8007
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-14 18:19:24 +0530

    Update Jenkinsfile.groovy

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -1,33 +1,19 @@
-node ('testnode')
-{
-    stage('stage1')
-    {
-            checkout scm     
-    }
-    stage('stage2')
-    {
-        throttle(['test_1'])
-        {
-            node ('testnode') 
-            {   
-                checkout scm
-            
+def mavenHome = tool(name: 'maven', type: 'maven')
+withEnv([
+            'MAVEN_HOME=' + mavenHome,
+            "PATH=${mavenHome}/bin:${env.PATH}"
+        ]){
+                stage('checkout & build')
+                {
+                    throttle(['test_1'])
+                    {
+                        node ('testnode') 
+                        {   
+                            //sh "echo $mavenHome"
+                            checkout scm
+                            sh "mvn clean package"
+                        
+                        }
+                    }         
+                }
             }
-        }         
-    }
-    stage('stage3')
-    {
-        throttle(['test_1'])
-        {
-            node ('testnode') 
-            {   
-                checkout scm
-            
-            }
-        }         
-    }
-        stage('stage4')
-    {
-         sh "echo $M2_HOME"         
-    }
-}
