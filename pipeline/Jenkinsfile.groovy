5e305850f9da3588e6a1d2b976ddaded74354513
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-03-14 12:20:54 +0530

    cc

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -1,21 +1,8 @@
-//node ('testnode') 
-//{
-
-/*stage('stage1')
-{
-    //throttle(['test_1'])
-    //{
-        //node ('testnode') 
-        //{
-            checkout scm
-       //} 
-    //}
-}
-/*def mavenHome = tool(name: 'maven', type: 'maven')
+def mavenHome = tool(name: 'maven', type: 'maven')
 withEnv([
             'MAVEN_HOME=' + mavenHome,
             "PATH=${mavenHome}/bin:${env.PATH}"
-        ]){*/
+        ]){
                 stage('checkout & build')
                 {
                     throttle(['test_1'])
@@ -29,28 +16,4 @@ withEnv([
                         }
                     }         
                 }
-            //}
-/*stage('stage3')
-{
-    throttle(['test_1'])
-    {
-        node ('testnode') 
-        {
-            sh "touch one.java"
-            sh "pwd"
-        } 
-    }
-}
-
-stage('stage4')
-{
-   // throttle(['test_1'])
-   // {
-        //node ('testnode') 
-        //{
-            sh "touch two.java"
-            sh "pwd"
-       //} 
-    //}
-}
-}*/
+            }
